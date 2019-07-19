319fed81c0ab0e8827f544844d49eeccc6d3c81e
Victor Buevich <io.futuramer.devops@gmail.com>
2019-07-19 22:11:43 +0300

    maven packaging added

---
+++ run.cmd
@@ -0,0 +1,3 @@
+@ECHO OFF
+ECHO Select Requirements/ExampleSheet.xlsx
+java -jar target/rental-reports-builder.jar
--- src/main/java/io/futuramer/rental/reports/builder/ui/App.java
+++ src/main/java/io/futuramer/rental/reports/builder/ui/App.java
@@ -8,13 +8,15 @@ import javafx.scene.image.Image;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("rentalReportsBuilder.fxml"));
-        Font.loadFont(App.class.getResource("Lobster.ttf").toExternalForm(), 10);
-        Image icon = new Image(App.class.getResourceAsStream("report.png"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("rentalReportsBuilder.fxml")));
+        Font.loadFont(Objects.requireNonNull(getClass().getClassLoader().getResource("Lobster.ttf")).toExternalForm(), 10);
+        Image icon = new Image(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("report.png")));
         primaryStage.getIcons().add(icon);
         primaryStage.setTitle("Rental Reports builder");
         primaryStage.setScene(new Scene(root, 700, 321));
